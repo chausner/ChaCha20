79245d965489a4ba57529a3bd0f7cfee59eb94ea
Christoph Hausner <christoph_hausner@hotmail.de>
2022-07-03 01:38:07 +0200

    Measure cycles per byte in benchmark

--- tests/src/benchmark.cpp
+++ tests/src/benchmark.cpp
@@ -1,11 +1,25 @@
 #include "chausner/chacha.hpp"
 
 #include <chrono>
+#include <cstdint>
 #include <cstdlib>
 #include <iostream>
 #include <vector>
 
 using namespace chausner;
+
+#ifdef _WIN32
+#include <intrin.h>
+uint64_t rdtsc() {
+    return __rdtsc();
+} 
+#else 
+uint64_t rdtsc() {
+    unsigned int low, high;
+    __asm__ __volatile__ ("rdtsc" : "=a" (low), "=d" (high));
+    return (static_cast<uint64_t>(high) << 32) | low;
+} 
+#endif
 
 template<int rounds>
 void benchmark(bool inplace, size_t buffer_size, size_t iterations) {
@@ -14,6 +28,7 @@ void benchmark(bool inplace, size_t buffer_size, size_t iterations) {
     Chacha<rounds> chacha(key.data(), nonce.data());
 
     auto start = std::chrono::high_resolution_clock::now();
+    auto start_cycles = rdtsc();
 
     std::vector<uint8_t> buffer(buffer_size);
 
@@ -28,14 +43,18 @@ void benchmark(bool inplace, size_t buffer_size, size_t iterations) {
         }
     }
 
+    auto elapsed_cycles = rdtsc() - start_cycles;
     auto elapsed = std::chrono::high_resolution_clock::now() - start;
     auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
     double mb_per_second = buffer.size() * iterations / 1024.0 / 1024.0 / (elapsed_ms / 1000.0);
+    double cycles_per_byte = static_cast<double>(elapsed_cycles) / (buffer_size * iterations);
 
-    std::cout << "Rounds: " << rounds << std::endl;
-    std::cout << "Inplace: " << (inplace ? "yes" : "no") << std::endl;    
-    std::cout << "Elapsed: " << elapsed_ms << "ms" << std::endl;
-    std::cout << "Speed: " << mb_per_second << "MB/s" << std::endl;
+    std::cout
+        << "Rounds: " << rounds
+        << ", inplace: " << (inplace ? "yes" : "no")   
+        << ", elapsed: " << elapsed_ms << "ms"
+        << ", speed: " << mb_per_second << "MB/s"
+        << ", cycles/byte: " << cycles_per_byte << std::endl;
 }
 
 void benchmark_all_variants(size_t buffer_size, size_t iterations) {
